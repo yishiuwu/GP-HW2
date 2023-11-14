bdc19d22276e362a9dc44a045c3e670914838092
yishiuwu <94129760+yishiuwu@users.noreply.github.com>
2023-11-14 21:02:06 +0800

    color effect

--- Assets/Scripts/Scene/ColorEffect.cs
+++ Assets/Scripts/Scene/ColorEffect.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
+[RequireComponent(typeof(Image))]
 public class ColorEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Image image;
     void Start()
     {
-        
+        image = GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator Fade(Color to, float duration, System.Action callback) {
+        float startTime = Time.time;
+        float t = (Time.time - startTime)/duration;
+        Color from = image.color;
+        while (t < 1) {
+            image.color = Color.Lerp(from, to, t);
+            yield return null;
+            t = (Time.time - startTime)/duration;
+        }
+        callback?.Invoke();
+    }
+
+    public void SetColor(Color color) {
+        image.color = color;
+    }
+    public void StartFade(Color to, float duration, System.Action callback = null) {
+        StartCoroutine(Fade(to, duration, callback));
     }
 }
--- Assets/Scripts/Scene/SceneTransition.cs
+++ Assets/Scripts/Scene/SceneTransition.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Object transImage;
+    
     void Start()
     {
         
