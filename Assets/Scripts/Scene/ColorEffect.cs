61e532c2c86d8dbc199b9e9de7eb0c82e3a8a156
yishiuwu <94129760+yishiuwu@users.noreply.github.com>
2023-11-15 01:54:03 +0800

    scene transition prefab

--- Assets/Scripts/Scene/ColorEffect.cs
+++ Assets/Scripts/Scene/ColorEffect.cs
@@ -1,34 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
-using Image = UnityEngine.UI.Image;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 public class ColorEffect : MonoBehaviour
 {
     private Image image;
-    void Start()
+    void Awake()
     {
         image = GetComponent<Image>();
+        // Debug.Log(image.ToString());
     }
 
     IEnumerator Fade(Color to, float duration, System.Action callback) {
         float startTime = Time.time;
         float t = (Time.time - startTime)/duration;
         Color from = image.color;
+        Debug.Log(t);
         while (t < 1) {
             image.color = Color.Lerp(from, to, t);
             yield return null;
             t = (Time.time - startTime)/duration;
+            // Debug.Log(t);
         }
         callback?.Invoke();
     }
 
     public void SetColor(Color color) {
+        // Debug.Log(color.ToString());
         image.color = color;
     }
     public void StartFade(Color to, float duration, System.Action callback = null) {
+        // Debug.Log("start fade");
         StartCoroutine(Fade(to, duration, callback));
     }
 }
